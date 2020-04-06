9796da13c5c123f9879c04c2ebdab9b290c32f31
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-04-05 19:54:11 -0600

    terminado

--- main.cpp
+++ main.cpp
@@ -9,12 +9,5 @@ using namespace std;
 int main(int argc, char** argv) {
 	Operaciones *op= new Operaciones();
 	op->menu();
-	/*for(int i = 0 ; i<12; i++){
-		for (int j = 0; j<12; j++){
-			if(i != j)
-				op->matriz->insertar(i,j, ".");
-		}
-	}
-	op->matriz->reporte();*/
 	return 0;
 }
